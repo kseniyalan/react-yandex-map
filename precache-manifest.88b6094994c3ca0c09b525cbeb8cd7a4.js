a05adaf6eb08dfe1fc000deb92e692cd64269e0a
Ksenia <k.lanetskaya@gmail.com>
2018-10-21 22:51:52 +0300

    Add build

---
+++ precache-manifest.88b6094994c3ca0c09b525cbeb8cd7a4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "59e1314589eb0eb428b7",
+    "url": "/react-yandex-map/static/js/runtime~main.59e13145.js"
+  },
+  {
+    "revision": "5dcbd2123fda85f30efa",
+    "url": "/react-yandex-map/static/js/main.5dcbd212.chunk.js"
+  },
+  {
+    "revision": "5fd3d219a5a6f4835b43",
+    "url": "/react-yandex-map/static/js/1.5fd3d219.chunk.js"
+  },
+  {
+    "revision": "2587b70b07ab0fb1b155f98b39d0e29e",
+    "url": "/react-yandex-map/index.html"
+  }
+];
