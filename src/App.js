7900b4c4eed2073dc711751e54169452d4be7f12
Ksenia <k.lanetskaya@gmail.com>
2018-10-21 22:18:22 +0300

    Add map logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './css/App.less';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+import Map from './Map.js';
 
-      </div>
-    );
-  }
+const Marker = () => false;
+Marker.propTypes = {
+	lat: PropTypes.number.isRequired,
+	lon: PropTypes.number.isRequired,
+};
+
+class App extends Component {
+	state = {
+		showMap: true,
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<button onClick={this.setState.bind(this, {showMap: !this.state.showMap}, null)}>Show/hide</button>
+				{this.state.showMap ? (
+					<Map>
+						<Marker lat={55.76} lon={37.64}/>
+					</Map>
+				) : false}
+			</div>
+		);
+	}
 }
 
 export default App;
